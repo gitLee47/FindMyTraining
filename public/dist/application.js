bf75a5215580a71b23702362fe0863b6cc4de801
Lee Sama <liju.leelives@gmail.com>
2015-10-15 21:49:37 +0530

    Build 4.0

--- public/dist/application.js
+++ public/dist/application.js
@@ -220,6 +220,8 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 
+		//setting twitter widget
+		twttr.widgets.load();
 
 		// Search Result
 		$scope.search = function() {
