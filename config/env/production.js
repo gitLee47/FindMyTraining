67d9a096b580e13a5b8bd5cbba087fe8bc9e3820
Lee Sama <liju.leelives@gmail.com>
2015-10-19 21:13:54 +0530

    Build 5.0

--- config/env/production.js
+++ config/env/production.js
@@ -7,22 +7,22 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.min.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-				'public/lib/angular-bootstrap-datetimepicker/src/css/datetimepicker.css'
+				'public/lib/angular-bootstrap-datetimepicker/src/css/datetimepicker.min.css'
 			],
 			js: [
 				'public/lib/angular/angular.min.js',
-				'public/lib/angular-resource/angular-resource.js', 
-				'public/lib/angular-cookies/angular-cookies.js', 
-				'public/lib/angular-animate/angular-animate.js', 
-				'public/lib/angular-touch/angular-touch.js', 
-				'public/lib/angular-sanitize/angular-sanitize.js', 
+				'public/lib/angular-resource/angular-resource.min.js',
+				'public/lib/angular-cookies/angular-cookies.min.js',
+				'public/lib/angular-animate/angular-animate.min.js',
+				'public/lib/angular-touch/angular-touch.min.js',
+				'public/lib/angular-sanitize/angular-sanitize.min.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
 				'public/lib/jquery/dist/jquery.min.js',
-				'public/lib/bootstrap/js/collapse.js',
+				'public/lib/bootstrap/js/collapse.min.js',
 				'public/lib/moment/min/moment.min.js',
-				'public/lib/angular-bootstrap-datetimepicker/src/js/datetimepicker.js'
+				'public/lib/angular-bootstrap-datetimepicker/src/js/datetimepicker.min.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
